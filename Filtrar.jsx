56d01adcabd1dee2fb2bea6957a9d8f703981b6c
RobertaDev1 <164791692+RobertaDev1@users.noreply.github.com>
2024-07-21 16:18:28 -0300

    Adicionado os elementos de Filtro e Busca
    Adicionando dois arquivos na pasta de componentes (Filtro e Busca)

---
+++ Filtrar.jsx
@@ -0,0 +1,24 @@
+const Filtrar = ({filter, setFilter}) => {
+  return (
+    <div className="filter">
+        <h2>Filtrar:</h2>
+        <div className="filter-options">
+            <div>
+                <p>Status:</p>
+                <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                    <option value="All">Todas</option>
+                    <option value="Completed">Completas</option>
+                     <option value="Incomplete">Incompletas</option>
+                </select>
+            </div>
+            <div>
+                <p>Ordem alfabética:</p>
+                <button>Crescente</button>
+                <button>Decrescente</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Filtrar
